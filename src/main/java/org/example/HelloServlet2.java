8a0eb7e2473312cce64505b84aa642e3d5b671f4
ohdury <ohdury@gmail.com>
2022-01-21 23:55:22 +0900

    동작하는 코드 커밋

---
+++ src/main/java/org/example/HelloServlet2.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/hello2")
+public class HelloServlet2 extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        super.doGet(req, resp);
+        resp.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = resp.getWriter();
+        out.println("hi, there?");
+        out.close();
+    }
+}
---
+++ src/main/webapp/hello.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: junggoonpark
+  Date: 2022/01/20
+  Time: 11:41 오후
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+    hello.jsp wow111
+</body>
+</html>
